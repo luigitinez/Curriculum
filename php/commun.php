db95fadeec6a34c2fc083f5e39be214d851f0900
Luis <lmgspain@hotmail.com>
2017-04-15 00:16:41 +0200

    Se actualiza el archivo, se añade la función que incluye styles. Esta función incluye todas las lineas que invocan a los estilos para que bootstrap y otros frameworks sean llamados correctamente y funcione correctamente el estilo visual.

--- php/commun.php
+++ php/commun.php
@@ -2,15 +2,13 @@
 
 function menu(){
 ?>
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse navbar-wrapper"">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand">Curriculum</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="index.php">Inicio</a></li>
-      <li><a href="#">Registrarse</a></li>
-      <li><a href="#">Log in</a></li>
     </ul>
     <form class="navbar-form navbar-left">
       <div class="input-group">
@@ -22,10 +20,21 @@ function menu(){
         </div>
       </div>
     </form>
-    
-    
+     <ul class="nav navbar-nav navbar-right">
+      <li><a href="reg.php" >Registrarse</a></li>
+      <li><a href="#" >Log in</a></li>
+    </ul>  
   </div>
-</nav><?php ;
+</nav>
+<?php ;
 }
 
+function styles(){?>
+  <link rel="stylesheet" type="text/css" href="">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><?
+}
 ?>
