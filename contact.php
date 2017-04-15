247b21e9c90c668008f0a0cab72713df488b6e78
Luis <lmgspain@hotmail.com>
2017-04-15 16:05:38 +0200

    Se ha añadido la carpeta media para albercar archivos multimedia tipo imagenes sonido etc. Se añade el formulario de contacto y su respectivo css, se añade un css exclusivo para el menu de registro. Se crea un favicon en media para usarlo en la pagina de registro

---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php 
+include "php/commun.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contacto</title>
+	<?php styles();?>
+	<link rel="stylesheet" type="text/css" href="css/style-contact.css">
+</head>
+<body>
+<? menu(); ?>
+<div class="container">
+
+
+    <div class="col-lg-offset-4 col-lg-4" id="panel">
+        <h2>Get In Touch</h2>
+
+        <form>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Name</label>
+            </div>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Email</label>
+            </div>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Mobile No</label>
+            </div>
+
+            <div class="group">
+                <input type="text" required>
+                <span class="highlight"></span>
+                <span class="bar"></span>
+                <label>Message</label>
+            </div>
+            <div class="group">
+                <center> <button type="submit" class="btn btn-warning">Send <span class="glyphicon glyphicon-send"></span></button></center>
+            </div>
+        </form>
+
+    </div>
+</div>
+</body>
+</html>
