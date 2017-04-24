5f9d3dbf1f88332ac85ccfe585029722ae429eb7
luigitinez <lmgspain@hotmail.com>
2017-04-24 23:40:31 +0200

    Se mueve log.php  a la carpeta php, ahora formará parte del core. Se encargará de procesar la información  enviada por el form y creará la sesión o redireccionará si no ha ido algo bien

---
+++ php/log.php
@@ -0,0 +1,6 @@
+<?php 
+
+/*realizar comprobaciones si el usuario existe*/
+/*si el usuario no es encontrado en la bbdd devolverlo de la página que viene*/
+header('Location: '.$_SERVER['HTTP_REFERER']); 
+?>
