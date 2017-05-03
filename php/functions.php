4dc2f3dc8a959885aed37efc9ca7cd2ed9fb5eb4
luigitinez <lmgspain@hotmail.com>
2017-05-03 23:46:04 +0200

    Se añaden funciones como turnback y se mejora geturl

--- php/functions.php
+++ php/functions.php
@@ -1,9 +1,17 @@
 <?php
 
-function geturl(){
+function geturl($navdir=""){
+	if($navdir=="")
 	$dir=$_SERVER['PHP_SELF'];
+	else
+	$dir=$navdir;
 	$pos=strrpos($dir,"/");
 	$pos=$pos+1;
 	$web=substr($dir, $pos);
 	return $web;
+}
+//esta funcion te devuelve a la url desde donde venias
+function turnback($params=""){
+
+	header('Location: '.$_SERVER['HTTP_REFERER']."?".$params); 
 }
