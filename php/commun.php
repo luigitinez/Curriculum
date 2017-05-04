a4b7366e60b8a05e0501daff4d651f985279dc1b
Luis <lmgspain@hotmail.com>
2017-05-04 16:36:32 +0200

    Cambios menores

--- php/commun.php
+++ php/commun.php
@@ -102,6 +102,10 @@ function styles(){?>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/style-one.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><?
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="icon" 
+      type="image/png" 
+      href="media/favicon.ico" />
+  <?
 }
 ?>
