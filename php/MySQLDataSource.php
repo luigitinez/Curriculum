e22cca540288cb39e1a00470d94e7af48e7e9fc2
luigitinez <lmgspain@hotmail.com>
2017-05-08 23:19:43 +0200

    Se ha creado la función regusr para generar inserts en la base de datos de usuarios.

--- php/MySQLDataSource.php
+++ php/MySQLDataSource.php
@@ -17,14 +17,29 @@ function usrexists ($mail){
   $query="SELECT * FROM `usr` WHERE `mail` = '".$mail."'";
   if(!$result =$enlace->query($query)){
    //la consulta no se ha realizado con exito
+   $mysqli->close();
    return false;
 
   }else {
         if ($result->num_rows === 0) {
+            $enlace->close();
             return false;//la consulta ha devuelto 0 filas, no existe el usuario
         }else{
+            $enlace->close();
             return $result;//usuario existe
         }
     }
 }
+
+function regusr($mail,$pass,$name,$surname){
+    $sqlins="INSERT INTO `usr`( `mail`, `pass`, `name`, `surname`) VALUES ('".$mail."','".$pass."','".$name."','".$surname."')";
+    $mysqli=conectar();
+    if ($resultado = $mysqli->query($sqlins)) {//todo fue bien
+        return true;
+    } else {//devolver error catastrofico
+        return false;
+        //registrar en el log el error de base de datos //echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
 ?>
