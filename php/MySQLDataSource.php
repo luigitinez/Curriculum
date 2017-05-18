e0fb49539a7fdb4373fabf03900141f2f91f6c3a
luigitinez <lmgspain@hotmail.com>
2017-05-18 20:56:05 +0200

    Se han añadido nuevas funciones ala bbdd para poder conectar con tabla profesiones

--- php/MySQLDataSource.php
+++ php/MySQLDataSource.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectar(){
-
+  //  @$connect = new mysqli("danigody001.mysql.guardedhost.com","danigody001_luismg","qqC-9kU-2LB-pS8","danigody001_cvproject");
     @$connect = new mysqli("localhost","root","","curriculum");
     if($connect->connect_errno){
        printf("<h1><span>LA CONEXIÓN CON LA BASE DE DATOS HA FALLADO: %s\n".$connect->connect_error."</span></h1>");
@@ -15,9 +15,8 @@ function usrexists ($mail){
     $enlace= conectar();
     //generamos una consulta para enviar a la bbdd
         //    SELECT `mail`, `pass`,`name`,`surname`,`admin`,`pic`, `nombre_profesion` FROM `usr` INNER JOIN `profesion` WHERE usr.FK_id_prof = profesion.id_prof AND usr.mail = 'lmgspain@hotmail.com'
-  $query = "SELECT `mail`, `pass`,`name`,`surname`,`admin`,`pic`, `nombre_profesion` FROM `usr` INNER JOIN `profesion` WHERE usr.FK_id_prof = profesion.id_prof AND usr.mail = '".$mail."'";
+  $query = "SELECT `mail`, `pass`,`name`,`surname`,`admin`,`pic`,`FK_id_prof`, `nombre_profesion` FROM `usr` INNER JOIN `profesion` WHERE usr.FK_id_prof = profesion.id_prof AND usr.mail = '".$mail."'";
   //$query="SELECT * FROM `usr` WHERE `mail` = '".$mail."'";
-  echo "<pre>".$query."</pre>";
   if(!$result =$enlace->query($query)){
    //la consulta no se ha realizado con exito
    $mysqli->close();
@@ -55,7 +54,25 @@ function getusr($mail){
     $row = $result->fetch_assoc();
     return $row;
     }
-    
 }
+
+function profesiones(){
+    $conn= conectar();
+     $sql = 'SELECT * FROM `profesion`';
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $profesiones=array();
+        while($row = $result->fetch_assoc()) {
+            /*recorremos todas las profesiones y las metemos en un array, donde la key será el id y el nombre la posición 
+            Esto lo hacemos por si se borra alguna profesion y la secuencia de ids no es continua no provoque errores*/
+            $profesiones[$row["id_prof"]]=$row["nombre_profesion"];
+        }
+        return $profesiones;
+    } else {
+        return false;
+    }
+    $conn->close();
+}
+
 
 ?>
