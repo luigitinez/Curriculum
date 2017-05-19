19729037668d55ddff98d99aad3357a528ccae58
luigitinez <lmgspain@hotmail.com>
2017-05-20 01:24:10 +0200

    se mejora el formulario de editar perfil para poder en un futuro modificar el perfl del usuario

--- edit.php
+++ edit.php
@@ -15,22 +15,33 @@ session_start();
 
 <div class="container">
     <h2>Editar Perfil</h2>
-        <div class="form-group">
-            <h4><span class="label label-default">Nombre:</span></h4>
-           <h3><span class="label label-info"><?= $_SESSION['usr']->getname()." ".$_SESSION['usr']->getsurname();?></span></h3>
-        </div>
-        <div class="form-group">
-            <h4><span class="label label-default">Correo:</span></h4>
-           <h3><span class="label label-info"><?= $_SESSION['usr']->getmail()?></span></h3>
-        </div>
+    <!--Se muestra tabla de datos no editables-->
+<table class="table noborder">
+    <tr><td> <h4><span class="label label-default">Nombre:</span></h4></td>     <td> <h4><span class="label label-default">Correo:</span></h4></td> </tr>
+    <tr><td> <h3><span class="label label-info"><?= $_SESSION['usr']->getname()." ".$_SESSION['usr']->getsurname();?></span></h3></td>       <td> <h3><span class="label label-info"><?= $_SESSION['usr']->getmail()?></span></h3></td></tr>
+</table>
+    <!-- fin tabla -->
+    
+      
     <form action="" method="POST">
-        
+        <input type="hidden" value=<?= "'".$_SESSION['usr']->getid()."'"; ?>>
+                                    
         <div class='form-group'>
-            <lable class="label label-default">Profesión:</lable>
+           <h4><lable class="label label-default">Profesión:</lable></h4>
                 <?php makeselect(); ?>
         </div>
-
-            <input type="submit" class="btn pull-right">
+        <fieldset><legend>Cambiar Contraseña</legend>
+        
+         <h4><span class="label label-default">Contraseña:</span></h4>
+        <div class='form-group'>
+            <input type="password" name="pass" class="form-control">
+        </div>
+            <h4><span class="label label-default">Repetir Contraseña:</span></h4>
+            <div class='form-group'>
+                <input type="password" name="repeat" class="form-control" >
+            </div>
+        </fieldset>
+        <input type="submit" class="btn pull-right">
     </form>
 </div>
 
